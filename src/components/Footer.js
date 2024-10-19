020dd3895f2c9ba7f682ae53420736e64b9e5ebd
Saivenkat Pentakota <saivenkatpentakota@gmail.com>
2024-10-19 10:14:02 +0530

    created Footer.js

---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <p>Subscribe to our newsletter</p>
+      <p>© 2024 Shop. All rights reserved.</p>
+    </footer>
+  );
+};
+
+export default Footer;
